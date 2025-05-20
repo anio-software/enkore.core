d07f2f32eeca521b4a9d621a720faf50250a7b64
eXory2024 <exory@anio.software>
2025-05-20 03:14:45 +0200

    No longer publish in @asint scope.

--- .cicd/deploy.sh
+++ .cicd/deploy.sh
@@ -3,9 +3,6 @@
 if [[ "$RELEASE_VERSION" == vp* ]]; then
 	npm publish --provenance --access public
 else
-	node ./.cicd/updatePackageName.mjs "@asint/enkore__core"
-	npm publish --access public
-
 	node ./.cicd/updatePackageName.mjs "@anio-software/enkore.core"
 	npm publish --access public
 fi
