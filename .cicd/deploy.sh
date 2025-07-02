4750342f51359d9ad29095bd1e1463e3f3794fa4
eXory2024 <exory@anio.software>
2025-07-02 21:47:57 +0200

    Remove .cicd, .github and auto.

--- .cicd/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -euf
-
-if [[ "$RELEASE_VERSION" == vp* ]]; then
-	npm publish --provenance --access public
-else
-	node ./.cicd/updatePackageName.mjs "@anio-software/enkore.core"
-	npm publish --access public
-fi
