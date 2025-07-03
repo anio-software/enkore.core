946a757521cc505bcb3dfd5701fd710aadd56ca5
eXory2024 <exory@anio.software>
2025-07-03 18:00:01 +0200

    __aggregatedExports was a hack used when the project was built using fourtune.

--- project/export/__aggregatedExports.ts
+++
@@ -1,9 +0,0 @@
-export const apiID = "EnkoreCoreAPI"
-export const apiMajorVersion = 0
-export const apiRevision = 0
-
-export {findProjectRootFromDirectory} from "#~src/public/findProjectRootFromDirectory.ts"
-export {createTemporaryFile} from "#~src/public/createTemporaryFile.ts"
-export {createTemporaryDirectory} from "#~src/public/createTemporaryDirectory.ts"
-export {initializeProject} from "#~src/public/initializeProject.ts"
-export {loadToolchain} from "#~src/public/loadToolchain.ts"
